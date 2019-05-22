6a9df238e1116dd0a6d91e209b2e64f8dbec1f34
Kirill Dvoynikov <illdve16@gmail.com>
2019-05-22 18:43:56 +0300

    added brain-even

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import greet from '..';
+import game from '..';
 
-console.log('Welcome to the Brain Games!');
-greet();
+game();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
 import readlineSync from 'readline-sync';
 
+
+let gameСircle = 3;
+const minInteger = 1;
+const maxInteger = 20;
+const YES = 'yes';
+const NO = 'no';
+const CORRECT = 'Correct!';
+const answersList = [YES, NO];
+
+const randomInteger = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+
+const isEven = (n) => {
+  const checkIsEven = n % 2 === 0;
+  return checkIsEven ? YES : NO;
+};
+
+const outputChecking = (correctAnswer, userAnswer) => {
+  const wrongAnswer = `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`;
+  const isRightAnswer = answersList.find(el => el === userAnswer) === correctAnswer;
+
+  if (!isRightAnswer) {
+    gameСircle = 0;
+    return wrongAnswer;
+  }
+  gameСircle -= 1;
+  return CORRECT;
+};
+
 export default () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no"  \n');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
+
+  while (gameСircle > 0) {
+    const integer = randomInteger(minInteger, maxInteger);
+    console.log(`Question: ${integer}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLocaleLowerCase();
+    const correctAnswer = isEven(integer);
+    const outputString = outputChecking(correctAnswer, userAnswer);
+    console.log(`${outputString} \n`);
+    const isSuccessRun = gameСircle < 1 && outputString === CORRECT;
+    if (isSuccessRun) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
 };
