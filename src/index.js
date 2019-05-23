62c568951bce0686d1ad5366d41436764a82eacf
Kirill Dvoynikov <illdve16@gmail.com>
2019-05-23 20:35:35 +0300

    refactor brain-even

--- src/index.js
+++ src/index.js
@@ -4,29 +4,13 @@ import readlineSync from 'readline-sync';
 let gameСircle = 3;
 const minInteger = 1;
 const maxInteger = 20;
-const YES = 'yes';
-const NO = 'no';
-const CORRECT = 'Correct!';
-const answersList = [YES, NO];
+
 
 const randomInteger = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
 
-const isEven = (n) => {
-  const checkIsEven = n % 2 === 0;
-  return checkIsEven ? YES : NO;
-};
+const isEven = n => n % 2 === 0;
+const answerCalc = n => (isEven(n) ? 'yes' : 'no');
 
-const outputChecking = (correctAnswer, userAnswer) => {
-  const wrongAnswer = `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`;
-  const isRightAnswer = answersList.find(el => el === userAnswer) === correctAnswer;
-
-  if (!isRightAnswer) {
-    gameСircle = 0;
-    return wrongAnswer;
-  }
-  gameСircle -= 1;
-  return CORRECT;
-};
 
 export default () => {
   console.log('Welcome to the Brain Games!');
@@ -35,15 +19,20 @@ export default () => {
   console.log(`Hello, ${name}!`);
 
   while (gameСircle > 0) {
-    const integer = randomInteger(minInteger, maxInteger);
-    console.log(`Question: ${integer}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLocaleLowerCase();
-    const correctAnswer = isEven(integer);
-    const outputString = outputChecking(correctAnswer, userAnswer);
-    console.log(`${outputString} \n`);
-    const isSuccessRun = gameСircle < 1 && outputString === CORRECT;
-    if (isSuccessRun) {
-      console.log(`Congratulations, ${name}!`);
+    const questionValue = randomInteger(minInteger, maxInteger);
+    console.log(`Question: ${questionValue}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = answerCalc(questionValue);
+
+    const isRightAnswer = userAnswer === correctAnswer;
+    if (!isRightAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      break;
     }
+    gameСircle -= 1;
+    console.log('Correct!');
+  }
+  if (gameСircle <= 0) {
+    console.log(`Congratulations, ${name}!`);
   }
 };
